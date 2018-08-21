5b2a9cf052209d880cf19bb915149bf74e869b22
Александр Питасов <alexpit63@gmail.com>
2018-08-21 11:39:33 +0300

    Выполняется задание 1. Разработан и опробован класс "работник", реализован вывод и экранирование. В следующем коммите должно быть 4 объекта типа "работник", вывод должен стать форматированным. Попробовать разобраться с выводом в консоль знака валюты.

--- Csharp_LabRab4/Program.cs
+++ Csharp_LabRab4/Program.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Csharp_LabRab4
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            /* 
+/* 
             ЗАДАНИЕ 1
             Вывести в консоль информацию о сотрудниках в виде таблицы(использовать экранирование символов).
             Колонки: имя сотрудника, название компании(в кавычках), дата устройства на работу(день.месяц.год), сумма продаж(decimal c символом валюты).
@@ -20,8 +10,60 @@ namespace Csharp_LabRab4
             Используя класс StringBuilder сформировать текст, состоящий из 100 строк.Каждая строка длиной от 20 до 100 случайных символов. Записать полученный результат в переменную типа string, вывести результат в консоль.
             */
 
-            // ЗАДАНИЕ 1
+// ЗАДАНИЕ 1
 
+namespace Csharp_LabRab4
+{
+    public class Worker {
+        string name;                //имя сотрудника
+        string company;             //имя компании
+        DateTime employment_data;   //дата устройства на работу - определить для каждого объекта через конструктор
+        string employment_data_str; //дата устройства на работу в формате строки
+        double salesRUB;            //сумма продаж в рублях
+        double salesEUR;            //сумма продаж в евро
+        char RUB = '₽';             //символ рубля
+        char EUR = '€';             //символ евро
+        string salesRUBStr;         //формируемая строка суммы продаж в рублях
+        string salesEURStr;         //формируемая строка суммы прожаж в евро
+
+        public Worker(string n, string c, int y, int m, int d, double sR, double sE) { //конструктор класса
+            name = n;
+            company = c;
+            employment_data = new DateTime(y, m, d);
+            salesRUB = sR;
+            salesEUR = sE;
+            salesRUBStr = Convert.ToString(salesRUB) + Convert.ToString(RUB);
+            salesEURStr = Convert.ToString(salesEUR) + Convert.ToString(EUR);
+            employment_data_str = employment_data.ToShortDateString();
+        }
+
+        public string getWorkerInfo(int param) {   //параметр вывода строки: 1 - выводит продажи в рублях, все остальное - в рублях
+            string result;      //результат формирования строки
+            if (param == 1)
+                result = name + ", " + company + ", " + employment_data_str + ", " + salesEURStr;
+            else
+                result = name + ", " + company + ", " + employment_data_str + ", " + salesRUBStr;
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Worker vanya = new Worker("Ваня", "\"Доза\"", 2018, 8, 1, 100000.50, 0);
+                Console.WriteLine("Имя    |  Компания  |   Дата устройства   |  Продажи");
+                Console.WriteLine(vanya.getWorkerInfo(1));
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString()); 
+            }
+            finally{
+                Console.Write("Press <Enter>");
+                Console.ReadLine();
+            }
         }
     }
 }
